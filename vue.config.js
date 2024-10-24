0aafc7a315d09df8bae9fd535f33fa327f26119f
yuanxue <yuanxue@sensetime.com>
2024-10-24 14:25:52 +0800

    feat: 更换tenser

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,14 @@ const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = defineConfig({
   publicPath: "./",
+  terser: {
+    minify: 'swc',
+  },
   configureWebpack: {
     cache: { type: "filesystem" },
     plugins: [new MonacoWebpackPlugin({
       languages: ['typescript', 'css', 'less', 'scss', 'html', 'javascript'],
+      globalAPI: true,
     })],
   },
 });
